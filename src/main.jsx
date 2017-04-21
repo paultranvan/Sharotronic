f9210012377e5ec21d06abb2d7ae3a6637b6b1d6
Gara64 <paul@cozycloud.cc>
2017-04-21 19:13:57 +0200

    Add cozy bar and cozy client

--- src/main.jsx
+++ src/main.jsx
@@ -12,29 +12,20 @@ const context = window.context
 const lang = document.documentElement.getAttribute('lang') || 'en'
 
 document.addEventListener('DOMContentLoaded', () => {
-    const context = window.context
-    const root = document.querySelector('[role=application]')
-    const data = root.dataset
-
+    const data = document.querySelector('[role=application]').dataset
     cozy.client.init({
-        //cozyURL: '//' + data.cozyDomain,
-        cozyURL: 'http://cozy1.local:8080',
-        oauth: {
-            clientParams: {/*...*/},
-            scopes: ["io.cozy.files:GET"],
-            onRegistered: (client, url) => { /* */ },
-            //storage: new cozy.auth.LocalStorage(window.localStorage)
-        }
-        //token: data.cozyToken
+        cozyURL: '//' + data.cozyDomain,
+        token: data.cozyToken
     })
 
     cozy.bar.init({
-        appName: 'sharing',
-        lang: data.cozyLocale
+        appName: data.cozyAppName,
+        iconPath: data.cozyIconPath
     })
-    render((
-        <I18n context={context} lang={lang}>
-            <App />
-        </I18n>
-    ), document.querySelector('[role=application]'))
+
+  render((
+    <I18n context={context} lang={lang}>
+        <App />
+    </I18n>
+  ), document.querySelector('[role=application]'))
 })
