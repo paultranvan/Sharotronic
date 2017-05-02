7f894c52adef8ff6e381b0b9cc705e565f6372fa
Gara64 <paul@cozycloud.cc>
2017-05-02 18:10:35 +0200

    Add tips

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,13 @@ import { translate } from '../lib/I18n'
 import classNames from 'classnames'
 import AddRecipient from './AddRecipient'
 import Share from './ShareForm'
+import Tips from './Tips'
 
 const App = ({ t }) => (
     React.createElement('div', {},
         React.createElement(Share, {}),
-        React.createElement(AddRecipient, {})
+        React.createElement(AddRecipient, {}),
+        React.createElement(Tips, {})
     )
 )
 
